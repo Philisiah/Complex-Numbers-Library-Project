a946c87791e649fb2a79e9e0a077e84ca1a6a8ff
Philisiah <sphilisiah@gmail.com>
2016-02-05 02:44:28 +0300

    Added more methods to support equality, reverse operations

--- mathx/complex.py
+++ mathx/complex.py
@@ -1,16 +1,7 @@
 ''' A class Complex that evaluates operations of complex numbers'''
 
 
-'''def conv(self):
-        #Checks for cases where
-        other is not a complex number#
-        if isinstance(other, (float, int)):
-            other = Complex(other)
-        else:
-            return False'''
-
-
-class Complex:
+class Complex(object):
 
     def __init__(self, rl=0, img=0.0):
         self.rl = rl
@@ -26,6 +17,7 @@ class Complex:
         else:
             return Complex(self.rl + other.rl,
                            self.img + other.img)
+    __radd__ = __add__  # reverse add remains the same for
 
     def __sub__(self, other):
         '''modifies the __sub__ method to control
@@ -38,6 +30,7 @@ class Complex:
         else:
             return Complex(self.rl - other.rl,
                            self.img - other.img)
+    __rsub__ = __sub__
 
     def __div__(self, other):
         '''modifies the __add__ method to control
@@ -47,9 +40,9 @@ class Complex:
         elif isinstance(other, float):
             return Complex(self.rl / other, self.img / other)
         else:
-            s = other.rl ** 2 + other.img ** 2
-            return Complex(self.rl * other.rl - self.rl * other.img/s
-                           + self.rl * other.rl + self.img * other.img/s)
+            s = float(other.rl ** 2 + other.img ** 2)
+        return Complex((self.rl * other.rl + self.img * other.img) / s,
+                       (self.img * other.rl - self.rl * other.img) / s)
 
     def __mul__(self, other):
         '''modify the __mul__ method to control
@@ -62,7 +55,17 @@ class Complex:
             return Complex(self.rl * other.rl + self.rl
                            * other.rl + self.img *
                            other.rl + self.img * other.img)
+    __rmul__ = __mul__  # reverse product remains the same
+
+    def __neg__(self):
+        '''this function handles
+         computations where one operand is negative'''
+        return Complex(-self.rl, -self.img)
+
+    def __ne__(self, other):
+        '''this specifies that conditions for which != will apply'''
+        return Complex(self.rl != other.rl, self.img != other.img)
 
     def __str__(self):
-        '''returns a printable string of the variables in an object '''
+        '''returns a printable string of the variables in an object to user'''
         return '(%g, %g)' % (self.rl, self.img)
