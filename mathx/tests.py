fb3a8f5bbe98171f82393caa260c2bdc18dd9dd5
Philisiah <sphilisiah@gmail.com>
2016-02-05 11:40:45 +0300

    test module

---
+++ mathx/tests.py
@@ -0,0 +1,17 @@
+
+
+from complex import Complex
+
+y = Complex(5, 10) / Complex(5, 2)
+x = Complex(20, 15) * Complex(12, 14.0)
+z = Complex(45, 15) + 2
+a = Complex(32, 20) - 5
+b = Complex(10, 3) + Complex(7, 2)
+c = Complex(42, 25) - Complex(45, 10)
+print c = b
+print c
+print y
+print x
+print z
+print a
+print b
