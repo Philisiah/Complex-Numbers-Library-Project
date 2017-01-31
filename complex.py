c5c51746447a3781fe8e137fd845678d0fcc787c
Philisiah <sphilisiah@gmail.com>
2017-01-31 15:43:57 +0300

    included checklist

--- complex.py
+++ complex.py
@@ -77,4 +77,10 @@ class Complex(object):
 		self._illegal('<=')
 
 	print 'ready for use'
-
+#checklist functions
+# ['__abs__', '__add__', '__class__', '__coerce__', '__delattr__', '__div__', '__divmod__', '__doc__', '__eq__',
+# '__float__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__gt__', '__hash__'
+# , '__init__', '__int__', '__le__', '__long__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__',
+# '__nonzero__', '__pos__', '__pow__', '__radd__', '__rdiv__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__r
+# epr__', '__rfloordiv__', '__rmod__', '__rmul__', '__rpow__', '__rsub__', '__rtruediv__', '__setattr__', '__size
+# of__', '__str__', '__sub__', '__subclasshook__', '__truediv__', 'conjugate', 'imag', 'real']
